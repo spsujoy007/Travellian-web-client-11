54ea38fa879e65093079f2208cb58e824561ff41
Sujoy Paul <freelancersujoy@gmail.com>
2022-11-08 21:42:16 +0600

    review section added

--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const Reviews = () => {
+const Reviews = ({service}) => {
     return (
         <div>
-            
+            <h1 className='uppercase text-3xl mt-10 py-4'>Reviews about {service.title} journey</h1>
+            <div className='flex items-center'>
+            <div className='flex items-center'>
+                <img style={{width: '60px'}} src="https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?b=1&s=612x612&w=0&k=20&c=IJ1HiV33jl9wTVpneAcU2CEPdGRwuZJsBs_92uk_xNs=" alt="" />
+                <h2 className='mx-3'>Mr jhankar</h2>
+            </div>
+
+            {/* form input review  */}
+            <div>
+                <form className='flex items-center'>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full  mr-5"/>
+                    <button type='submit' className='btn btn-primary'>Post</button>
+                </form>
+            </div>
+            </div>
         </div>
     );
 };
--- src/Pages/SingleService/SingleService.js
+++ src/Pages/SingleService/SingleService.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Reviews from "../Reviews/Reviews";
 
 const SingleService = () => {
-    const service = useLoaderData();
-    const {img, title, description, price} = service;
-    return (
-        <div className='py-14'>
-            <div className="card card-side bg-base-100 ">
-  <img src={img} className="w-96 rounded-xl" alt="Movie"/>
-  <div className="card-body">
-    <h2 className="card-title">{title}</h2>
-    <p>{description}</p>
-    <h3 className='text-xl'>Price: ${price}</h3>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Watch</button>
-    </div>
-  </div>
-</div>
+  const service = useLoaderData();
+  const { img, title, description, price } = service;
+  return (
+    <div className="py-14">
+      <div className="card card-side bg-base-100 ">
+        <img src={img} className="w-96 rounded-xl" alt="Movie" />
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>{description}</p>
+          <h3 className="text-xl">Price: ${price}</h3>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Watch</button>
+          </div>
         </div>
-    );
+      </div>
+
+      <Reviews
+        service={service}
+      ></Reviews>
+    </div>
+  );
 };
 
 export default SingleService;
