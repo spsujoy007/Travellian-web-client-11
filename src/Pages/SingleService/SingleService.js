d59abd78be54ef7764e0a16420ee2cf0a18a7ee6
Sujoy Paul <freelancersujoy@gmail.com>
2022-11-09 11:27:55 +0600

    react image zoom in part complete

--- src/Pages/SingleService/SingleService.js
+++ src/Pages/SingleService/SingleService.js
@@ -1,20 +1,28 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import Reviews from "../Reviews/Reviews";
 import { PhotoProvider, PhotoView } from 'react-photo-view';
 import 'react-photo-view/dist/react-photo-view.css';
+import { FaAngleLeft } from "react-icons/fa";
 
 const SingleService = () => {
   const service = useLoaderData();
-  const { img, title, description, price } = service;
+  const {img, title, description, price } = service;
   return (
     <div className="py-14">
-      <div className="card card-side bg-base-100 ">
+
+    <div className="flex md:justify-end mb-5">
+      <Link to='/services'>
+      <button className="btn btn-outline btn-primary btn-circle btn-sm "><FaAngleLeft></FaAngleLeft></button>
+      </Link>
+    </div>      
+
+      <div className="card md:card-side bg-base-100  flex">
       <PhotoProvider>
       <PhotoView src={img}>
-        <img src={img} className="w-96 rounded-xl cursor-zoom-in" alt="Movie" />
+        <img src={img} className="w-full md:w-96 rounded-xl cursor-zoom-in" alt="Movie" />
       </PhotoView>
-    </PhotoProvider>
+      </PhotoProvider>
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
           <p>{description}</p>
